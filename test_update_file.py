d48f176a5b5364c6519f46ae370ea841f4c357c8
pirimovs-itelvo <pirimovs1@gmail.com>
2024-10-10 10:09:12 +0300

    Delete test_update_file.py

--- test_update_file.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def main():
-    print("hdsi!")
-
-if __name__ == "__main__":
-    # Run the update script before starting the main program
-    subprocess.run(['python', 'update.py'])
-    main()
